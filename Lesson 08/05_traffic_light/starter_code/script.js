d035ef3c6ff5aa3c0e609c1eb06a4006d9b6d42f
JoshBlyden <Joshuasblyden@gmail.com>
2022-10-05 22:11:07 -0400

    stoplight lesson

--- Lesson 08/05_traffic_light/starter_code/script.js
+++ Lesson 08/05_traffic_light/starter_code/script.js
@@ -1,2 +1,25 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
+
+
+// Identify Stop light
+// When stop is clicked, turn on red stop light
+// AND deactivate Slow light and Go Light
+
+
+
+
+
+
+// Identify Slow Light
+// When slow light is clicked, turn on yellow slow light
+// AND deactive Stop light and Go Light
+
+
+
+
+
+
+// Identify Go Light
+// When go light is clicked, turn on green go light
+// AND deactiveate Slow light and Stop Light
