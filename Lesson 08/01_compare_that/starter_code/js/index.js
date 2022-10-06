c4657ee0fd1e35a4fe12d3bc5f346a970a147eb0
JoshBlyden <Joshuasblyden@gmail.com>
2022-10-05 22:02:48 -0400

    lesson 8

--- Lesson 08/01_compare_that/starter_code/js/index.js
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -5,8 +5,25 @@
      let a = Number(document.querySelector('#a').value);
      let b = Number(document.querySelector('#b').value);
  
+     console.log(a);
+     console.log(b);
      // Add comparison logic
- 
+     
+     // Are they not numbers
+     // Are they equal
+     // Is A greater than B
+     // Is B greater than A
+
+
+     if(isNaN(a) || isNaN(b)){
+      comparison = "N/A";
+     }else if(a > b){
+      comparison = ">";
+     }else if(a == b){
+      comparison = "=";
+     }else if(a < b){
+      comparison = "<";
+      }
      document.querySelector('#comparison').innerText = comparison;
    };
  
