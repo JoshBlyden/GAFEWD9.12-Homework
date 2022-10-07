fb25b6eeccb51564c382b5deee925f3d6684a867
JoshBlyden <Joshuasblyden@gmail.com>
2022-10-06 22:43:13 -0400

    hw4updates

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -2,6 +2,19 @@
  * DOTS: Level One
  *
  */
+
+/* 
+ *
+ *
+ *
+  ## Level 1
+  For level 1, implement a single event listener to the `.js-ball` element. When the user clicks on the ball, it should increment their score by 10 points. When the user reaches 100 points, they've beat the level and can move on!  
+ *
+ *
+ *
+ */
+
+
 let score = 0;
 let ballEl = document.querySelector('.js-ball');
 let scoreEl = document.querySelector('.js-score');
@@ -9,3 +22,24 @@ let scoreEl = document.querySelector('.js-score');
 function declareWinner() {
   document.body.classList.add('game-over');
 }
+
+// Create a function for adding +10 to the score
+
+function plusTEN(){
+  score += 10;
+  scoreEl.innerText = score;
+}
+
+ballEl.addEventListener('click',plusTEN) 
+
+
+
+
+
+
+
+//When ball is clicked, launch funciton to add 10 points
+
+
+
+//Create a rule that when the score is zero, the game end
--- Lesson 08/05_traffic_light/starter_code/script.js
+++ Lesson 08/05_traffic_light/starter_code/script.js
@@ -2,24 +2,88 @@
 // You'll use addEventListener and getElementById if you're doing it right...
 
 
-// Identify Stop light
+// STOP LIGHT -------------------------------
+
+// Identify Stop light button
+const stopLightBtn = document.querySelector("#stopButton")
+
+//Identify stop light bulb
+const stopLightBulb = document.querySelector(" #stopLight");
+
+// function to add light and remove light
+function turnStopLightOn(){
+    clearLights() // Deactivates all lights
+    stopLightBulb.classList.add('redBulb');
+} 
+
+function turnStopLightOff(){
+    stopLightBulb.classList.remove('redBulb');
+}
+
+
 // When stop is clicked, turn on red stop light
-// AND deactivate Slow light and Go Light
+stopLightBtn.addEventListener('click', turnStopLightOn);
+
+// When bulb is clicked, turn off light
+stopLightBulb.addEventListener('click',turnStopLightOff);
 
 
 
 
+// SLOW LIGHT -------------------------------
 
+// Identify Slow Light button
+const slowLightBtn = document.querySelector("#slowButton")
 
-// Identify Slow Light
+// Identify slow light bulb
+const slowLightBulb = document.querySelector(" #slowLight");
+
+// function to add light, and remove light
+function turnSlowLightOn(){
+    clearLights() // Deactivates all lights
+    slowLightBulb.classList.add('yellowBulb');
+} 
+
+function turnSlowLightOff(){
+    slowLightBulb.classList.remove('yellowBulb');
+}
+
 // When slow light is clicked, turn on yellow slow light
-// AND deactive Stop light and Go Light
+slowLightBtn.addEventListener('click', turnSlowLightOn);
+
+// When bulb is clicked, turn off light
+slowLightBulb.addEventListener('click',turnSlowLightOff);
 
 
+// GO LIGHT -------------------------------
 
+// Identify Go Light button
+const goLightBtn = document.querySelector("#goButton")
+// Identify go light bulb
+const goLightBulb = document.querySelector(" #goLight");
 
+// function to add light and remove light
+function turnGoLightOn(){
+    clearLights() // Deactivates all lights
+    goLightBulb.classList.add('greenBulb');
+} 
 
+function turnGoLightOff(){
+    goLightBulb.classList.remove('greenBulb');
+}
 
-// Identify Go Light
 // When go light is clicked, turn on green go light
-// AND deactiveate Slow light and Stop Light
+goLightBtn.addEventListener('click', turnGoLightOn);
+
+// When bulb is clicked, turn off light
+goLightBulb.addEventListener('click',turnGoLightOff);
+
+// --------------------------------------
+
+// clear lights function
+
+function clearLights(){
+    stopLightBulb.classList.remove('redBulb')
+    slowLightBulb.classList.remove('yellowBulb')
+    goLightBulb.classList.remove('greenBulb')
+}
