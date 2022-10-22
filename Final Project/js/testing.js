120e5549a3c6921f0ed4c5b18283dc7bff98a90f
JoshBlyden <Joshuasblyden@gmail.com>
2022-10-22 17:46:34 -0400

    project

--- Final Project/js/testing.js
+++ Final Project/js/testing.js
@@ -3,30 +3,18 @@ let pCount = 798;
 let populationCount = document.querySelector('.populationCounter');
 const signUpBtn = document.querySelector('.ctaMain');
 
-
-
-
-
 /*Ignore primary link function*/
 signUpBtn.onclick = function(e) {
     e.preventDefault()
-    console.log("This too")
 };
-
 
 function plusOne() {
     pCount +=1;
     populationCount.innerText = pCount;
-    console.log("This Part Works")
 };
-
-
-
-
 
     /* When clicking "Join the City" the population tracker will increase by 1 */
 signUpBtn.addEventListener('click',plusOne);
-signUpBtn.addEventListener('click', console.log("Finally, this"));
 
 
 
